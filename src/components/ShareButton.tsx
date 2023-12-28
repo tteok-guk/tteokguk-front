e63bdd522a12f2690e818b4724eba4f7d3ccf5ae
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-12-29 03:21:46 +0900

    feat: 니떡국 내떡국 UI분기처리 및 니떡국 버튼이벤트 추가 (TG-8)

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,22 +1,36 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import { BottomButton } from './common'
 
 const ShareButton = () => {
   const handleCopyClipBoard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text)
-
       // alert('복사 성공!');
     } catch (error) {
       alert('복사 실패!')
     }
   }
+  const pathname = usePathname()
+
   return (
-    <BottomButton
-      fullBtnName="덕담 남기기"
-      fullBtnClick={() => handleCopyClipBoard('https://develop-tteokguk.vercel.app/니떡국')}
-    />
+    <>
+      {pathname === '/my' ? (
+        <BottomButton
+          fullBtnName="덕담 남기기"
+          fullBtnClick={() => handleCopyClipBoard('https://develop-tteokguk.vercel.app/니떡국')}
+        />
+      ) : (
+        <BottomButton
+          split="twice"
+          smallBtnName="내떡국"
+          fullBtnName="덕담 남기기"
+          smallBtnHref="/my"
+          fullBtnHref="/dsda/write"
+        />
+      )}
+    </>
   )
 }
 
