8a4b7bc4cc5c0b318bb7b559bd342268a5ece4a8
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2023-12-31 02:32:11 +0900

    Fix: BottomButton height 통일, position 변경 및 bg-color 추가

--- src/components/common/BottomButton.tsx
+++ src/components/common/BottomButton.tsx
@@ -13,29 +13,31 @@ export default function BottomButton({
   fullBtnHref,
 }: BottomButtonProps) {
   return (
-    <div className="fixed bottom-0 left-0 flex h-117 w-full gap-x-12 bg-bg px-20 pt-16">
-      {split === 'twice' ? (
-        <>
-          <Button
-            href={smallBtnHref}
-            className="min-h-51 min-w-51 max-w-100 flex-shrink-0 flex-grow border border-pr-500 text-pr-500"
-            onClick={smallBtnClick}
-          >
-            {smallBtnName}
-          </Button>
-          <Button
-            href={fullBtnHref}
-            className="min-h-51 min-w-218 flex-shrink-0 flex-grow border bg-pr-500 text-white"
-            onClick={fullBtnClick}
-          >
+    <div className="fixed bottom-0 left-0 h-117 w-full bg-bg pt-16">
+      <div className="mx-auto flex h-full min-w-320 max-w-575 justify-center gap-x-12 px-20">
+        {split === 'twice' ? (
+          <>
+            <Button
+              href={smallBtnHref}
+              className="h-58 min-w-51 max-w-100 flex-shrink-0 flex-grow border border-pr-500 text-pr-500"
+              onClick={smallBtnClick}
+            >
+              {smallBtnName}
+            </Button>
+            <Button
+              href={fullBtnHref}
+              className="h-58 min-w-218 flex-shrink-0 flex-grow border bg-pr-500 text-white"
+              onClick={fullBtnClick}
+            >
+              {fullBtnName}
+            </Button>
+          </>
+        ) : (
+          <Button href={fullBtnHref} size="full" className="border" onClick={fullBtnClick}>
             {fullBtnName}
           </Button>
-        </>
-      ) : (
-        <Button href={fullBtnHref} size="full" className="border" onClick={fullBtnClick}>
-          {fullBtnName}
-        </Button>
-      )}
+        )}
+      </div>
     </div>
   )
 }
