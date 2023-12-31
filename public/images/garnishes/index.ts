9a98c4fad2cce769818082f2780296b06e4adf76
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-01 02:42:05 +0900

    Docs: 기본 고명 이미지 임시 추가, 이미지별 폴더 설정 (TG-10)

---
+++ public/images/garnishes/index.ts
@@ -0,0 +1,37 @@
+import basicRc from './basicRc.png'
+import cheese from './cheese.png'
+import coloredRc from './coloredRc.png'
+import coriander from './coriander.png'
+import cucumber from './cucumber.png'
+import dumpling from './dumpling.png'
+import egg from './egg.png'
+import greenOnion from './greenOnion.png'
+import heartEgg from './heartEgg.png'
+import meat from './meat.png'
+import mushroom from './mushroom.png'
+import pepperoncino from './pepperoncino.png'
+import pineapple from './pineapple.png'
+import seaweed from './seaweed.png'
+import shapedRc from './shapedRc.png'
+import strawberry from './strawberry.png'
+import vegetable from './vegetable.png'
+
+export {
+  basicRc,
+  cheese,
+  coloredRc,
+  coriander,
+  cucumber,
+  dumpling,
+  egg,
+  greenOnion,
+  heartEgg,
+  meat,
+  mushroom,
+  pepperoncino,
+  pineapple,
+  seaweed,
+  shapedRc,
+  strawberry,
+  vegetable,
+}
