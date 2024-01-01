d8af7be7a89c928c2c4d5c532a980597d0f76d71
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-02 04:34:41 +0900

    Design: 공통 input, textarea 기본 css 수정 (TG-26)

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-40 w-full rounded-md border border-input bg-background px-12 py-8 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'font-lg font-sm flex w-full border-b border-[#ADADAD] bg-transparent p-4 caret-pr-500 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
