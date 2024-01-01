06e77c11e6b2a6b11299af9367c05ffa87ef6c89
godjooyoung <xolovesjoo36@naver.com>
2024-01-02 02:20:09 +0900

    Design: 회원가입 화면 기본 레이아웃

--- src/app/(sign-up)/join/page.tsx
+++ src/app/(sign-up)/join/page.tsx
@@ -1,3 +1,18 @@
 export default function JoinPage() {
-  return <div>닉넴입력하기</div>
+  return (
+    <div>
+      <div>
+        {/* 화살표, 단계 페이지네이션 */}
+        상단영역
+      </div>
+      <div>
+        <div>
+          {/* 회원가입 단계 안내 영역 */}
+          <h1>내 떡국에 표시될<br/>닉네임을 만들어 주세요</h1>
+          <h2>최대 8자 / 공백, 특수기호 불가</h2>
+        </div>
+      </div>
+    </div>
+    
+  )
 }
