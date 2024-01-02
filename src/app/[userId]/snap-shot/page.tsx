98c6ad61957e87e0af568db40e3b7b718c770a3a
stella0905 <109677566+stella0905@users.noreply.github.com>
2024-01-03 00:45:22 +0900

    Fix: 에러확인(TG-32)

--- src/app/[userId]/snap-shot/page.tsx
+++ src/app/[userId]/snap-shot/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import { iconClose } from '../../../../public/images/icons'
-import { avatar1, defaultAvatar } from '../../../../public/images/avatar'
-import React from 'react'
-import ShareButton from '@/components/ShareButton'
 import SaveAsImageHandler from '@/components/SaveAsImageHandler'
 
 const snapShotPage = () => {
