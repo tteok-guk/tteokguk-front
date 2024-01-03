c5facb3ab7dec3a888d5e7b0bbd9544fe0b9eb01
stella0905 <109677566+stella0905@users.noreply.github.com>
2024-01-04 00:49:58 +0900

    feat: 캡쳐시 이미지 잘림 해결(TG-32)

--- src/components/SaveImage.tsx
+++ src/components/SaveImage.tsx
@@ -9,18 +9,16 @@ const SaveImage = () => {
   return (
     <>
       <div className="flex-center flex flex-row ">
-        <Image
-          src={`/images/avatar/${tempData.hostAvatar}.png`}
-          width={125}
-          height={158}
-          alt="hostAvatar"
-        />
-        <Image
-          src={`/images/avatar/${tempData.visitorAvatar}.png`}
-          width={125}
-          height={158}
-          alt="visitorAvatar"
-        />
+        <div className="relative h-158 w-125">
+          <Image src={`/images/avatar/${tempData.hostAvatar}.png`} layout="fill" alt="hostAvatar" />
+        </div>
+        <div className="relative h-158 w-125">
+          <Image
+            src={`/images/avatar/${tempData.visitorAvatar}.png`}
+            layout="fill"
+            alt="visitorAvatar"
+          />
+        </div>
       </div>
       <div className="flex-center mb-95 flex flex-row gap-13">
         <div className=" flex-center flex h-108 w-108 rounded-full border bg-gr-500">{'떡국'}</div>
