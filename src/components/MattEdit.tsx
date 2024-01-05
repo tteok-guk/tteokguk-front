4ebb87ea2cc5d30b3c196a620c74a88694b442a7
stella0905 <109677566+stella0905@users.noreply.github.com>
2024-01-05 18:17:12 +0900

    remove: 필요없는 이미지 삭제 (TG-48)

--- src/components/MattEdit.tsx
+++ src/components/MattEdit.tsx
@@ -7,7 +7,7 @@ const MattEdit = ({ mattType }: MattEditType) => {
   return (
     <div className="flex-center flex flex-col">
       <button
-        className={` mb-4 h-60 w-60 rounded-full bg-cover bg-center shadow-md ${miniMattObj[mattType]}`}
+        className={` mb-4 h-60 w-60 rounded-full bg-cover bg-center shadow-md ${mattObj[mattType]}`}
       />
       <p className="font-sm ">매트변경</p>
     </div>
