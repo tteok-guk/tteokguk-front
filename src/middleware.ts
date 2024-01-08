00404f39cb959e6e1292a09db33bf7ec0cd5a726
godjooyoung <xolovesjoo36@naver.com>
2024-01-09 02:26:49 +0900

    Feat: 미들웨어, 유저 상태 확인 함수 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,98 +1,99 @@
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { getUserType } from "@/services/userCheck";
 
 export async function middleware(request: NextRequest) {
-  console.log('[middle]', request.nextUrl.pathname)
-  console.log('[middle]', request.nextUrl.searchParams)
+  // 접근 불가 URL 차단 + 접근 가능 URL? => 쿠키값 확인 => 사용자 타입 확인 타입별 valid 체크
   const path = request.nextUrl.pathname
-  const params = request.nextUrl.searchParams
-  const cookie = request.cookies
-  
-  const goNext = () => {
-    return NextResponse.next()
-  }
-  
-  const goUrl = (url: string) => {
-    return NextResponse.redirect(new URL(url, request.url))
-  }
-
-//   const auth = async () => {
-//     if(cookie.has('token')){
-//       const token = cookie.get('token')?.value
-//       if(await getUserType(token)){
-//         return {'hasToken':true, 'isMember':true}
-//       }else{
-//         return {'hasToken':true, 'isMember':false}
-//       }
-//     }else{
-//       return {'hasToken':false, 'isMember':false}
-//     }
-//   }
-//   const authData = await auth()
-
-//   if(path.startsWith('/host')){
-//     // access denied url
-//     // TODO 유리님 개발 완료하고 나면 host/snap-shot도 막기
-//     if(path.includes('/set-garnish') || path.includes('/write') /*|| path.includes('/snap-shot')*/){
-//       return goUrl('/')
-//     }else{
-//       if(authData.hasToken){
-//         if(authData.isMember){
-//           return goNext()
-//         }else{
-//           return goUrl('/join')
-//         }
-//       }else{
-//         return goUrl('/')
-//       }
-//     }
-//   }else{
-//     // access denied url
-//     if(path.includes('/garnish-list')){
-//       return goUrl('/')
-//     }else if(path.includes('/join')){
-//       if(authData.hasToken){
-//         if(authData.isMember){
-//           return goUrl('/host')
-//         }else{
-//           return goNext()
-//         }
-//       }else{
-//         return goUrl('/')
-//       }
-//     }else if(path.includes('change-matt') || path.includes('/make-dish') || path.includes('/account')){
-//       if(authData.hasToken){
-//         if(authData.isMember){
-//           return goNext()
-//         }else{
-//           return goUrl('/join')
-//         }
-//       }else{
-//         return goUrl('/')
-//       }
-//     }else if (path.startsWith('/login/success')){
-//       if(params.get('token')){
-//         const token = params.get('token')
-//         request.cookies.set({
-//           name:'token', 
-//           value: token?token:'',
-//         })
-
-//         if((await getUserType(token)).isMember){
-//           return goUrl('/host')
-//         }else{
-//           return goUrl('/join')
-//         }
-//       }else{
-//         return goUrl('/')
-//       }
-//     }
-//   }
+  /*
+    Access Denied URL
+    [domain]/host/set-garnish
+    [domain]/host/write
+    [domain]/host/snap-shot  TODO 유리님 개발 완료하고 나면 막기
+    [domain]/!host//garnish-list
+  */
+  if(path.startsWith('/host')){
+    if(path.includes('/set-garnish') || path.includes('/write') /*|| path.includes('/snap-shot')*/){
+      // 접근 불가 URLs
+      return NextResponse.redirect(new URL('/', request.url))
+    }else{
+      // 쿠키 확인
+      const token = request.cookies.get('token')?.value
+      if(token){
+        // 사용자 타입 확인
+        const userType =  await getUserType(token)
+        if(userType.data.isMember){
+          return NextResponse.next()
+        }else{
+          return NextResponse.redirect(new URL('/', request.url)).cookies.delete('token')
+        }
+      }else{
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+    }
+  }else{
+    if(path.includes('/garnish-list')){
+      if(request.cookies.get('token')?.value){
+        return NextResponse.redirect(new URL('/', request.url)).cookies.delete('token')
+      }else{
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+    }else if(path.startsWith('/join')){
+      // 쿠키 확인
+      const token = request.cookies.get('token')?.value
+      if(token){
+        // 사용자 타입 확인
+        const userType =  await getUserType(token)
+        if(userType.data.isMember){
+          return NextResponse.redirect(new URL('/', request.url)).cookies.delete('token')
+        }else{
+          return NextResponse.next()
+        }
+      }else{
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+    }else if(path.startsWith('/change-matt') || path.startsWith('/make-dish') || path.startsWith('/account')){
+      // 쿠키 확인
+      const token = request.cookies.get('token')?.value
+      if(token){
+        // 사용자 타입 확인
+        const userType =  await getUserType(token)
+        if(userType.data.isMember){
+          return NextResponse.next()
+        }else{
+          return NextResponse.redirect(new URL('/', request.url)).cookies.delete('token')
+        }
+      }else{
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+    }else if (path.startsWith('/login/success')){
+      if(request.nextUrl.searchParams.get('token')){
+        // 쿼리파람 - 토큰
+        const token = request.nextUrl.searchParams.get('token')
+        // 사용자 타입 확인
+        const userType =  await getUserType(token)
+        if(userType.data.isMember){
+          // 쿠키에 토큰 세팅
+          const response = NextResponse.redirect(new URL('/host', request.url))
+          response.cookies.set('token', token || '')
+          return response
+        }else{
+          const response = NextResponse.redirect(new URL('/join', request.url))
+          response.cookies.set('token', token || '')
+          return response
+        }
+      }else{
+        const response = NextResponse.redirect(new URL('/', request.url))
+        if(request.cookies.has('token')){
+          response.cookies.delete('token')
+        }
+        return response
+      }
+    }
+    }
   }
 
 export const config = {
-  // 전역에서 체크하려다가 너무 많이 호출 되서 바꿈.
   matcher: [
     '/login/success', '/host/:path*', '/join', '/change-matt', '/make-dish', '/account'
   ]
