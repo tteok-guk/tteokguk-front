225d0aad2fa07da8bb4a78e393e31680c7fa9cc6
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-10 03:22:45 +0900

    Fix : PC/MO 체크 CSR로 실행되도록 수정 (TG-28)

--- src/utils/isMobileDevice.ts
+++ src/utils/isMobileDevice.ts
@@ -1,20 +1,22 @@
 export const isMobileDevice = () => {
-  const userDevice = window.navigator.userAgent
-  const mobileRegex = [
-    /iPhone/i,
-    /iPad/i,
-    /iPod/i,
-    /Android/i,
-    /BlackBerry/i,
-    /nokia/i,
-    /windows ce/i,
-    /Windows Phone/i,
-    /webOS/i,
-    /IEMobile/i,
-    /Opera Mini/i,
-    /Opera Mobi/i,
-    /Xiaomi/i,
-    /Huawei/i,
-  ]
-  return mobileRegex.some((mobile) => userDevice.match(mobile))
+  if (typeof window !== 'undefined') {
+    const userDevice = window.navigator.userAgent
+    const mobileRegex = [
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /Android/i,
+      /BlackBerry/i,
+      /nokia/i,
+      /windows ce/i,
+      /Windows Phone/i,
+      /webOS/i,
+      /IEMobile/i,
+      /Opera Mini/i,
+      /Opera Mobi/i,
+      /Xiaomi/i,
+      /Huawei/i,
+    ]
+    return mobileRegex.some((mobile) => userDevice.match(mobile))
+  }
 }
