1da836f7379bce4458b33ba9e829408c5bbd9ccf
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-09 09:45:28 +0900

    Docs: 캐릭터 일러스트 파일 추가 (TG-54)
    Co-Authored-By: Shinheeje <107026311+Shinheeje@users.noreply.github.com>
    Co-Authored-By: 신주영 <58963027+godjooyoung@users.noreply.github.com>
    Co-Authored-By: stella0905 <109677566+stella0905@users.noreply.github.com>

--- public/images/avatar/index.ts
+++ public/images/avatar/index.ts
@@ -1,4 +1,39 @@
 import avatar1 from './avatar1.png'
 import defaultAvatar from './defaultAvatar.png'
+import cat from './cat.png'
+import catArm from './catArm.png'
+import dog from './dog.png'
+import dogArm from './dogArm.png'
+import dragon from './dragon.png'
+import dragonArm from './dragonArm.png'
+import hedgehog from './hedgehog.png'
+import hedgehogArm from './hedgehogArm.png'
+import otter from './otter.png'
+import otterArm from './otterArm.png'
+import panda from './panda.png'
+import pandaArm from './pandaArm.png'
+import rabbit from './rabbit.png'
+import rabbitArm from './rabbitArm.png'
+import squirrle from './squirrle.png'
+import squirrleArm from './squirrleArm.png'
 
-export { defaultAvatar, avatar1 }
+export {
+  defaultAvatar,
+  avatar1,
+  cat,
+  catArm,
+  dog,
+  dogArm,
+  dragon,
+  dragonArm,
+  hedgehog,
+  hedgehogArm,
+  otter,
+  otterArm,
+  panda,
+  pandaArm,
+  rabbit,
+  rabbitArm,
+  squirrle,
+  squirrleArm,
+}
