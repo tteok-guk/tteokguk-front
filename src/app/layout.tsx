12f074eb4f058660c4c34f7183a4322c76efdaf6
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-10 01:44:33 +0900

    Design: 전체 폰트 컬러 gr-900 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <ReactQueryProvider>
           <RecoilProvider>
-            <main className="layout-scroll relative mx-auto h-full min-w-320 max-w-575 overflow-y-auto bg-bg px-20 pt-32">
+            <main className="layout-scroll relative mx-auto h-full min-w-320 max-w-575 overflow-y-auto bg-bg px-20 pt-32 text-gr-900">
               {children}
             </main>
             <div id="modal-root"></div>
