98986a6ac11f9d8f382c07e5c95d17bd1c8c298e
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-10 01:47:20 +0900

    [TG-55] 변경된 일러스트 이미지 반영, 전체 폰트 컬러 설정

--- public/images/avatar/index.ts
+++ public/images/avatar/index.ts
@@ -1,39 +1,7 @@
-import avatar1 from './avatar1.png'
-import defaultAvatar from './defaultAvatar.png'
-import cat from './cat.png'
-import catArm from './catArm.png'
-import dog from './dog.png'
-import dogArm from './dogArm.png'
-import dragon from './dragon.png'
-import dragonArm from './dragonArm.png'
-import hedgehog from './hedgehog.png'
-import hedgehogArm from './hedgehogArm.png'
-import otter from './otter.png'
-import otterArm from './otterArm.png'
-import panda from './panda.png'
-import pandaArm from './pandaArm.png'
-import rabbit from './rabbit.png'
-import rabbitArm from './rabbitArm.png'
-import squirrle from './squirrle.png'
-import squirrleArm from './squirrleArm.png'
+// todo 추후 임시 아바타 삭제 예정
 
-export {
-  defaultAvatar,
-  avatar1,
-  cat,
-  catArm,
-  dog,
-  dogArm,
-  dragon,
-  dragonArm,
-  hedgehog,
-  hedgehogArm,
-  otter,
-  otterArm,
-  panda,
-  pandaArm,
-  rabbit,
-  rabbitArm,
-  squirrle,
-  squirrleArm,
-}
+import avatar1 from './avatar1.png'
+import cryingAvatars from './cryingAvatars.png'
+import defaultAvatar from './defaultAvatar.png'
+
+export { avatar1, cryingAvatars, defaultAvatar }
