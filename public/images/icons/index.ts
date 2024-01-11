086c556ea157daddf8b8089e6fdb3b0ece1ef66b
shinheeje <dbfk2637@naver.com>
2024-01-11 20:51:21 +0900

    Fix : 배포에러 확인용

--- public/images/icons/index.ts
+++ public/images/icons/index.ts
@@ -2,7 +2,7 @@ import iconArrow from './iconArrow.png'
 import iconClose from './iconClose.png'
 import iconDday from './iconDday.png'
 import iconMypage from './iconMypage.png'
-import iconArrow2 from './iconArrow2.png'
 import iconError from './iconError.png'
+import iconArrow2 from './iconArrow2.png'
 
 export { iconArrow, iconClose, iconDday, iconMypage, iconArrow2, iconError }
