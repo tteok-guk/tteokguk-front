a23a4dff37ab23e4a2846a31fc572daae2910fd5
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-13 17:24:00 +0900

    Feat : 모달(Portal) 분기처리 설정 (TG-57)
    - props로 내려오는 type값에 따라 그에 맞는 모달 컴포넌트가 열리도록 설정

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { createPortal } from 'react-dom'
+import { useEffect, useState } from 'react'
+import { ModalProps, ModalType } from '@/types/CommonTypes'
+import { ConfirmModal, RouletteModal } from '../modal'
+
+export default function Modal({ type, cancelClick, confirmClick }: ModalProps) {
+  const [mounted, setMounted] = useState(false)
+
+  const componentType: ModalType = {
+    confirm: <ConfirmModal cancelClick={cancelClick} confirmClick={confirmClick} />,
+    roulette: <RouletteModal cancelClick={cancelClick} />,
+  }
+  const componentToRender = componentType[type]
+  const modalRoot = document.getElementById('modal-root') as HTMLElement
+
+  useEffect(() => {
+    setMounted(true)
+    return () => setMounted(false)
+  }, [])
+
+  return mounted ? createPortal(componentToRender, modalRoot) : <></>
+}
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 import TopButton from './TopButton'
 import BottomButton from './BottomButton'
 import PaginationEntire from './PaginationEntire'
+import Modal from './Modal'
 
-export { TopButton, BottomButton, PaginationEntire }
+export { TopButton, BottomButton, PaginationEntire, Modal }
