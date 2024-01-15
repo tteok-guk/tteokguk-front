4d350871307b7a4b330d7e3e9cb1423d90eabac3
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-16 01:01:53 +0900

    Feat : 룰렛 canvans → img 수정 (TG-73)

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -1,25 +1,29 @@
 'use client'
 
-import { useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { useRecoilState } from 'recoil'
 import { rouletteResultState } from '@/store/WriteAtom'
 import { OptionGarnishes } from '../../data/garnishRoulette'
-import NextImage from 'next/image' // HTML Image와 이름 중복되어 별칭으로 import
+import Image from 'next/image'
 import { Button } from './ui/button'
 import { iconLocation } from '../../public/images/icons'
+import { rouletteBoard } from '../../public/images/favicon'
 
 export default function Roulette() {
   const [rouletteResult, setRouletteResult] = useRecoilState(rouletteResultState) // 룰렛 결과값
+  const [disabled, setDisabled] = useState(false)
 
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const imageRef = useRef<HTMLImageElement>(null)
 
   // * 룰렛 회전 함수
   const rotate = () => {
-    const canvas = canvasRef.current
-    if (!canvas) return
+    const image = imageRef.current
+    if (!image) return
 
-    canvas.style.transition = 'initial'
-    canvas.style.transform = 'initial'
+    setDisabled(true) // 버튼 1회만 클릭 가능하도록 state 설정
+
+    image.style.transition = 'initial'
+    image.style.transform = 'initial'
 
     const TOTAL_ROTATION_TIME = 3000 // 룰렛 회전에 소요되는 시간
     const totalDegrees = 3600 // 총 회전할 각도
@@ -35,8 +39,8 @@ export default function Roulette() {
     }
     rotationAngle += 81 // 룰렛 보정 각도
 
-    canvas.style.transition = `transform ${TOTAL_ROTATION_TIME}ms ease-out`
-    canvas.style.transform = `rotate(-${totalDegrees + rotationAngle}deg)`
+    image.style.transition = `transform ${TOTAL_ROTATION_TIME}ms ease-out`
+    image.style.transform = `rotate(-${totalDegrees + rotationAngle}deg)`
 
     // 회전 후 랜덤 선택된 고명 설정
     setTimeout(() => {
@@ -44,95 +48,30 @@ export default function Roulette() {
     }, TOTAL_ROTATION_TIME)
   }
 
-  // * 이미지 로드 함수
-  const loadImg = (src: string) => {
-    return new Promise<HTMLImageElement>((resolve, reject) => {
-      const img = new Image()
-      img.onload = () => resolve(img)
-      img.onerror = reject
-      img.src = src
-    })
-  }
-
-  // * 컴포넌트가 마운트된 후 캔버스에 룰렛 생성
-  useEffect(() => {
-    // 이미지가 모두 onload 되었을 때 함수 실행
-    Promise.all(OptionGarnishes.map((item) => loadImg(item.src))).then((images) => {
-      const canvas = canvasRef.current // 캔버스 엘리먼트
-      if (!canvas) return
-
-      // 2D 그래픽을 그리기 위한 컨텍스트 객체(ctx)를 가져옴
-      const ctx = canvas.getContext('2d')
-      if (!ctx) return
-
-      // 캔버스의 높이와 절반 값을 계산하여 중앙점의 x좌표(cw)와 y좌표(ch)를 구함
-      const [cw, ch] = [canvas.width / 2, canvas.height / 2]
-
-      const radius = cw - 20 // 캔버스 중앙에서부터의 반지름 정의
-      let totalAngle = 0 // 총 각도 계산을 위한 초기값 설정
-
-      // OptionGarnishes의 각 아이템을 순회하며 룰렛의 섹터를 그림
-      OptionGarnishes.forEach((item, idx) => {
-        const angle = (item.proportion / 100) * (Math.PI * 2)
-
-        // 1) 섹터 그리기
-        // 시작 각도: totalAngle, 끝 각도: totalAngle + angle
-        ctx.beginPath()
-        ctx.fillStyle = item.color
-        ctx.moveTo(cw, ch)
-        ctx.arc(cw, ch, radius, totalAngle, totalAngle + angle)
-        ctx.fill()
-
-        // 2) 원형 테두리 그리기
-        ctx.beginPath()
-        ctx.arc(cw, ch, radius, 0, Math.PI * 2)
-        ctx.strokeStyle = '#007791'
-        ctx.lineWidth = 0.1
-        ctx.stroke()
-
-        // 3) 이미지 그리기
-        // 반지름의 3/4을 사용하여 이미지를 중앙(1/2)보다 더 바깥쪽으로 이동
-        const elementX = cw + ((radius * 3) / 4) * Math.cos(totalAngle + angle / 2)
-        const elementY = ch + ((radius * 3) / 4) * Math.sin(totalAngle + angle / 2)
-        const scaledWidth = 33
-        const scaledHeight = 33
-
-        // 각 이미지의 배경 그리기
-        // const backgroundRadius = scaledWidth / 2 + 10
-        // ctx.beginPath()
-        // ctx.fillStyle = item.color
-        // ctx.arc(elementX, elementY, backgroundRadius, 0, Math.PI * 2)
-        // ctx.fill()
-
-        // 이미지가 섹터의 중앙에 오도록 위치 조정
-        const adjustedImgX = elementX - scaledWidth / 2
-        const adjustedImgY = elementY - scaledHeight / 2
-
-        const img = images[idx]
-        ctx.drawImage(img, adjustedImgX, adjustedImgY, scaledWidth, scaledHeight)
-
-        // 4) 다음 섹터를 위한 시작 각도 업데이트
-        totalAngle += angle
-      })
-    })
-  }, [])
-
   return (
     <div className="relative">
-      <NextImage
+      <Image
         src={iconLocation}
         alt="선택된 고명을 가리키는 아이콘"
         width="25"
         height="28"
-        className="absolute left-140 top-5 z-10"
+        className="absolute left-127 top-3 z-10"
       />
-      <canvas ref={canvasRef} width="300" height="300"></canvas>
+      <Image
+        ref={imageRef}
+        src={rouletteBoard}
+        height={270}
+        width={270}
+        className="mt-20"
+        alt=""
+        priority
+      />
       <Button
         onClick={rotate}
-        disabled={!!rouletteResult}
+        disabled={disabled}
         className={`
-          absolute left-107 top-107 h-90 w-90 rounded-full font-soyoThin font-bold text-white
-          ${rouletteResult ? 'bg-gr-100' : 'bg-pr-500'}
+          absolute left-90 top-107 z-10 h-90 w-90 rounded-full font-soyoThin font-bold text-white
+          ${disabled ? 'bg-gr-100' : 'bg-pr-500'}
         `}
       >
         룰렛
