fb8c73cee3c16afffec10778ab50ab79f25abd6e
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-16 23:20:45 +0900

    Feat : 룰렛 확률 로직 가중치 적용하여 수정 (TG-88)

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -4,7 +4,9 @@ import { useState, useRef } from 'react'
 import { useRecoilState } from 'recoil'
 import { rouletteResultState } from '@/store/WriteAtom'
 import { OptionGarnishes } from '../../data/garnishRoulette'
+import { getRandomGarnish } from '@/utils/getRandomGarnish'
 import Image from 'next/image'
+import { toast } from '@/hooks/use-toast'
 import { Button } from './ui/button'
 import { iconLocation } from '../../public/images/icons'
 import { rouletteBoard } from '../../public/images/etc'
@@ -27,24 +29,31 @@ export default function Roulette() {
 
     const TOTAL_ROTATION_TIME = 3000 // 룰렛 회전에 소요되는 시간
     const totalDegrees = 3600 // 총 회전할 각도
-    const randomIdx = Math.floor(Math.random() * OptionGarnishes.length) // 랜덤 선택된 아이템의 인덱스
+    const { randomGarnish, randomIdx } = getRandomGarnish()
 
-    // OptionGarnishes 배열의 proportion을 기반으로 부채꼴(arcurate) 각도 계산
-    const arc = OptionGarnishes.map((item) => (item.proportion / 100) * 360)
+    if (randomGarnish === 'none') {
+      toast({
+        description:
+          '랜덤 고명 선택 중 오류가 발생했습니다. 팝업 종료 후 룰렛을 다시 클릭해주세요.',
+      })
+    }
+
+    // OptionGarnishes 배열의 weight을 기반으로 부채꼴(arcurate) 각도 계산
+    const arc = OptionGarnishes.map((item) => (item.weight / 100) * 360)
 
     // 랜덤 선택된 고명까지 회전시킬 각도 계산
     let rotationAngle = 0
     for (let i = 0; i <= randomIdx; i++) {
       rotationAngle += arc[i] // 0번째 인덱스부터 randomIdx까지 해당 각도값 누적
     }
-    rotationAngle += 81 // 룰렛 보정 각도
+    rotationAngle += 83 // 룰렛 보정 각도
 
     image.style.transition = `transform ${TOTAL_ROTATION_TIME}ms ease-out`
     image.style.transform = `rotate(-${totalDegrees + rotationAngle}deg)`
 
     // 회전 후 랜덤 선택된 고명 설정
     setTimeout(() => {
-      setRouletteResult(OptionGarnishes[randomIdx]['id'])
+      setRouletteResult(randomGarnish)
     }, TOTAL_ROTATION_TIME)
   }
 
@@ -53,16 +62,16 @@ export default function Roulette() {
       <Image
         src={iconLocation}
         alt="선택된 고명을 가리키는 아이콘"
-        width="25"
-        height="28"
-        className="absolute left-127 top-3 z-10"
+        width="28"
+        height="31"
+        className="absolute left-1/2 top-0 z-10 -translate-x-1/2"
       />
       <Image
         ref={imageRef}
         src={rouletteBoard}
         height={270}
         width={270}
-        className="mt-18"
+        className="mt-19"
         alt="룰렛 원판 이미지"
         priority
       />
