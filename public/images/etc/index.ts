52f6c911af62c448cd396f60fdeb4d20eb713597
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-16 10:03:34 +0900

    [TG-73] 룰렛 canvans → img 수정

--- public/images/etc/index.ts
+++ public/images/etc/index.ts
@@ -1,6 +1,7 @@
 import faviconMedium from './faviconMedium.png'
 import faviconSmall from './faviconSmall.png'
+import rouletteBoard from './rouletteBoard.svg'
 import serviceDragon from './serviceDragon.png'
 import servicePreviewDish from './servicePreviewDish.svg'
 
-export { faviconMedium, faviconSmall, serviceDragon, servicePreviewDish }
+export { faviconMedium, faviconSmall, rouletteBoard, serviceDragon, servicePreviewDish }
