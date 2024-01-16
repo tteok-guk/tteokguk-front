6f536e30fdd5ce10b33e07c1c25ba4b83179efd4
Shinheeje <107026311+Shinheeje@users.noreply.github.com>
2024-01-16 20:44:51 +0900

    [TG-7] 테이블 매트 변경
    * Feat : 메트변경하기 1차
    * Feat : 테이블 매트 변경 기능 완료
    * Design : 테이블 매트 변경페이지 떡국 이미지 변경

--- src/app/(my-page)/change-matt/page.tsx
+++ src/app/(my-page)/change-matt/page.tsx
@@ -1,105 +1,64 @@
+'use client'
 import { TopButton } from '@/components/common'
 import Image from 'next/image'
 import { basicDish } from '../../../../public/images/dishes'
-import {
-  miniBlueGradation,
-  miniBlueDew,
-  minipinkDew,
-  minipinkGradation,
-  miniPurpleCheck,
-  miniYellowCheck,
-} from '../../../../public/images/matts'
 import { BottomButton } from '@/components/common'
+import { AllMatt } from '../../../../data/change-matt'
+import { useState } from 'react'
+import { putMatt } from '@/services/changeMatt'
+import { useMutation } from '@tanstack/react-query'
+import { RequestParamType } from '@/types/apiTypes'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/hooks/use-toast'
+import { sampleDish } from '../../../../public/images/dishes'
 
 export default function ChangeMattPage() {
+  const [chosenMatt, setChosenMatt] = useState('')
+  const setMatt = (clickedValue: string) => setChosenMatt(clickedValue)
+  const router = useRouter()
+  // const { toast } = useToast()
+
+  const onChangeMatt = useMutation({
+    mutationFn: (chosenMatt: RequestParamType) => putMatt(chosenMatt),
+    onSuccess: (res) => {
+      // toast({ description: '매트가 변경되었습니다.' })
+      router.push(`/${res.data.tteokGukId}?page=1`)
+    },
+    onError: (err) => console.log('err', err),
+  })
+
+  const completeBtn = () => {
+    onChangeMatt.mutate({ mattType: chosenMatt })
+  }
   return (
     <>
-      <div className="bg h-364 bg-[url(/images/matts/purpleCheck.png)] bg-cover bg-no-repeat">
-        <TopButton />
-        <p className="font-xl">변경할 테이블 매트를</p>
-        <p className="font-xl">선택해 주세요</p>
-        <Image width={184} height={184} src={basicDish} alt="dish image" className="m-auto mt-20" />
+      <div className="h-dvh">
+        <div className="mx-[-20px] mt-[-32px] h-[55%] bg-[url(/images/matts/purpleCheck.png)] bg-cover bg-no-repeat px-20 pt-32">
+          <TopButton />
+          <p className="font-xl">
+            변경할 테이블 매트를 <br /> 선택해 주세요
+          </p>
+          <div className="absolute left-1/2 h-350 w-350 -translate-x-1/2 p-65">
+            <Image src={sampleDish} alt="dish image" className="" />
+          </div>
+        </div>
+        <div className="grid basis-1/4 grid-cols-4 grid-rows-2 gap-12">
+          {AllMatt.map((matt, idx) => (
+            <button key={idx} onClick={() => setMatt(matt.id)}>
+              <Image
+                width={95}
+                height={95}
+                src={matt.src}
+                alt={matt.alt}
+                className={`m-auto mt-20 ${
+                  chosenMatt === matt.id ? 'rounded-md border-3 border-pr-500' : ''
+                }`}
+              />
+            </button>
+          ))}
+        </div>
+        <BottomButton fullBtnName="완료" fullBtnClick={completeBtn} />
       </div>
-      {/* 메트이미지 클릭시 border 선택표시 활성화 */}
-      <div className="grid grid-cols-4 grid-rows-2 gap-x-12">
-        <button>
-          <Image
-            width={75}
-            height={75}
-            src={miniBlueDew}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          {' '}
-          <Image
-            width={75}
-            height={75}
-            src={miniPurpleCheck}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          <Image
-            width={75}
-            height={75}
-            src={minipinkDew}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          {' '}
-          <Image
-            width={75}
-            height={75}
-            src={minipinkGradation}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          <Image
-            width={75}
-            height={75}
-            src={miniBlueDew}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          {' '}
-          <Image
-            width={75}
-            height={75}
-            src={miniYellowCheck}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          <Image
-            width={75}
-            height={75}
-            src={minipinkDew}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-        <button>
-          {' '}
-          <Image
-            width={75}
-            height={75}
-            src={minipinkGradation}
-            alt="dish image"
-            className="m-auto mt-20"
-          />
-        </button>
-      </div>
-      <BottomButton fullBtnName="완료" />
     </>
   )
 }
