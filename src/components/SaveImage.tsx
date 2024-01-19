6ac7e14749b79383ecc4694a09e94b0784cf4bce
stella0905 <109677566+stella0905@users.noreply.github.com>
2024-01-19 21:52:47 +0900

    design: 윈도우 확인 test(TG-99)

--- src/components/SaveImage.tsx
+++ src/components/SaveImage.tsx
@@ -41,39 +41,37 @@ const SaveImage = ({ type, avatar, garnish }: snapShotType) => {
           </div>
         </div>
       </div> */}
-      <div className="relative  flex h-full w-full flex-col items-center justify-center px-2">
+      <div className="relative  flex  flex-col items-center justify-center px-2">
         <div className="relative block  w-full">
           <Image src={speechBubble} alt="말풍선" layout="responsive" />
         </div>
-        <div className="w-full">
-          <div className=" flex flex-row gap-30">
-            <div className=" relative z-[99] block h-160 w-1/2 scale-x-[-1]">
-              <Image
-                src={`/images/avatar/arm/${avatar.hostAvatar}Arm.png`}
-                width={164}
-                height={164}
-                layout="responsive"
-                alt="hostAvatar"
-              />
-            </div>
-            <div className="relative mx-[-55px] mt-[20%] block h-57 w-1/5">
-              <Image
-                src={`/images/garnishes/${garnish}.png`}
-                width={71}
-                height={57}
-                layout="responsive"
-                alt="garnishes"
-              />
-            </div>
-            <div className="relative block h-160 w-1/2 ">
-              <Image
-                src={`/images/avatar/arm/${visitorAvatar}Arm.png`}
-                width={164}
-                height={164}
-                layout="responsive"
-                alt="hostAvatar"
-              />
-            </div>
+        <div className=" flex w-full flex-row gap-30">
+          <div className=" relative z-[99] block h-160 w-1/2 scale-x-[-1]">
+            <Image
+              src={`/images/avatar/arm/${avatar.hostAvatar}Arm.png`}
+              width={164}
+              height={164}
+              layout="responsive"
+              alt="hostAvatar"
+            />
+          </div>
+          <div className="relative mx-[-55px] mt-[20%] block h-57 w-1/5">
+            <Image
+              src={`/images/garnishes/${garnish}.png`}
+              width={71}
+              height={57}
+              layout="responsive"
+              alt="garnishes"
+            />
+          </div>
+          <div className="relative block h-160 w-1/2">
+            <Image
+              src={`/images/avatar/arm/${visitorAvatar}Arm.png`}
+              width={164}
+              height={164}
+              layout="responsive"
+              alt="hostAvatar"
+            />
           </div>
         </div>
       </div>
