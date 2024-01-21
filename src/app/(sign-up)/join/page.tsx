a2268f9e732acd6818fa2317c88a817c7c42d5db
godjooyoung <xolovesjoo36@naver.com>
2024-01-22 03:54:26 +0900

    Design : 회원가입 디자인

--- src/app/(sign-up)/join/page.tsx
+++ src/app/(sign-up)/join/page.tsx
@@ -30,7 +30,7 @@ export default function JoinPage() {
   const validSt = 'border-b-[#ADADAD]'
 
   // button style
-  const activeBtnSt = 'font-semibold border-0 bg-pr-500 text-17 leading-22 text-white active:bg-pr-500'
+  const activeBtnSt = 'font-semibold border-0 bg-pr-500 text-17 leading-22 text-white active:bg-pr-500 cursor-pointer'
   const disabledBtnSt = 'font-semibold border-0 bg-gr-100 text-17 leading-22 text-gr-400 disabled:bg-gr-100 active:bg-gr-100 hover:bg-gr-100'
 
   // 하단 버튼 활성화 상태
@@ -246,8 +246,6 @@ export default function JoinPage() {
 
   // [step] useEffect Hook
   useEffect(() => {
-    console.log(">>>>>", step.current)
-    console.log(">>>>>", step.status)
     if (step.status[step.current] === StepStatus.COMPLETE) {
       setIsStepBtnActive(true)
     } else {
@@ -257,10 +255,10 @@ export default function JoinPage() {
 
 
   return (
-    <div className={step.current === 1 ? 'bg-[url(/images/matts/blueDew.png)] bg-cover bg-[-140px] mt-[-32px] mx-[-20px]':''}>
+    <div className={step.current === 1 ? 'bg-[url(/images/matts/blueDew.png)] bg-cover bg-[-140px] mt-[-32px] mx-[-20px] h-[calc(100%-117px)]':''}>
       {/* 상단 영역 */}
       <div className={step.current === 1 ?'flex pt-20 px-20':'flex mt-[-12px]'}>
-        <div className={step.current === 0 ? 'pl-0 pr-24 py-12 invisible' : 'pl-0 pr-24 py-12'} onClick={() => { navBtnOnClickHandler(step.current, 'prev') }}>
+        <div className={step.current === 0 ? 'pl-0 pr-24 py-12 invisible' : 'pl-0 pr-24 py-12 cursor-pointer'} onClick={() => { navBtnOnClickHandler(step.current, 'prev') }}>
           <Image src={iconArrow} alt="왼쪽을 가르키는 화살표 이미지" width={24} height={24} />
         </div>
         <div className={'flex gap-6 items-center  ml-[calc(50%-56px)]'}>
@@ -270,7 +268,7 @@ export default function JoinPage() {
         </div>
       </div>
       {/* 컨텐츠 영역 */}
-      <div>
+      <div className={'h-[calc(100%-35px)]'}>
         {/* step 0 */}
         <div className={step.current === 0 ? 'flex flex-col gap-38' : 'hidden'}>
           <div className={'flex flex-col gap-4'}>
@@ -285,26 +283,25 @@ export default function JoinPage() {
               className={isValidName ? `${iptSt}${validSt}` : `${iptSt}${invalidSt}`}
               onChange={(e) => (userNameOnChangeHandler(e))}
               value={userName} />
-            <div className={userName.length > 0 ? 'absolute right-0 top-4 w-fit' : 'hidden'} onClick={deleteNameOnClickHandler}>
+            <div className={userName.length > 0 ? 'absolute right-0 top-4 w-fit cursor-pointer' : 'hidden'} onClick={deleteNameOnClickHandler}>
               <Image src={iconCloseCircle} alt="인풋 내용 삭제 버튼 이미지" width={20} height={20}/>
             </div>
           </div>
         </div>
         {/* step 1 */}
-
-        <div className={step.current === 1 ? 'flex flex-col gap-8  px-20' : 'hidden'}>
+        <div className={step.current === 1 ? 'flex flex-col gap-8  px-20 h-full' : 'hidden'}>
           <div>
             <h1 className={'font-xl text-gr-900'}>캐릭터를 선택해주세요</h1>
           </div>
-          <div className={'flex flex-col gap-20'}>
+          <div className={'flex flex-col gap-20 h-full justify-between'}>
             <div className={'relative flex justify-center'}>
               <Image src={avatars[selectAvatar.idx].nomalSrc} alt="선택 캐릭터 이미지" width={255} height={255} loading='eager'/>
             </div>
-            <div className={step.current === 1 ?'relative grid grid-cols-4 grid-rows-2 justify-center gap-12  bg-bg mt-[-20px] mx-[-20px] pt-[20px] px-[20px]':'relative grid grid-cols-4 grid-rows-2 justify-center gap-12'}>
+            <div className={step.current === 1 ?'relative grid grid-cols-4 grid-rows-2 justify-center gap-12 bg-white mt-[-20px] mx-[-20px] pt-[20px] px-[20px]':'relative grid grid-cols-4 grid-rows-2 justify-center gap-12'}>
               {
                 avatars.map((avatar, idx) => {
-                  return <div onClick={()=>{selectAvatarOnClickHandler(avatar.name, idx)}} className={avatar.name === selectAvatar.name ? ' flex justify-center items-center min-w-75 min-h-75 aspect-square rounded-6 bg-pr-100 ring-pr-500 ring-[3px] ring-inset' : 'flex justify-center items-center min-w-75 min-h-75 aspect-square rounded-6 bg-pr-100'} key={idx} >
-                      <Image src={avatar.smallSrc} alt={avatar.alt} width={75} height={75} style={{width: '100%', height: 'auto',}} />
+                  return <div onClick={()=>{selectAvatarOnClickHandler(avatar.name, idx)}} className={avatar.name === selectAvatar.name ? ' flex justify-center items-center min-w-75 min-h-75 aspect-square rounded-6 bg-pr-100 ring-pr-500 ring-[3px] ring-inset cursor-pointer' : 'flex justify-center items-center min-w-75 min-h-75 aspect-square rounded-6 bg-pr-100 cursor-pointer'} key={idx} >
+                      <Image src={avatar.smallSrc} alt={avatar.alt} width={75} height={75} style={{width: '100%', height: 'auto',}} loading='eager' />
                     </div>
                 })
               }
@@ -319,7 +316,7 @@ export default function JoinPage() {
             <h1 className={'font-xl text-gr-900'}>니떡국 내떡국<br />서비스 이용에 동의해 주세요</h1>
           </div>
           <div className={'relative flex flex-col gap-y-20'}>
-            <div className={isStepBtnActive?'flex items-center w-full gap-10 py-16 pl-20 bg-pr-100 rounded-4 h-52':'flex items-center w-full gap-10 py-16 pl-20 bg-gr-100 rounded-4 h-52'}>
+            <div className={isStepBtnActive?'flex items-center w-full gap-10 py-16 pl-20 bg-pr-100 rounded-4 h-52 cursor-pointer':'flex items-center w-full gap-10 py-16 pl-20 bg-gr-100 rounded-4 h-52 cursor-pointer'}>
               <Checkbox id='termsAll' checked={groupTerm} onCheckedChange={groupCheckboxOnChangeHandler} className={'w-20 h-20 rounded-full bg-center border-0 bg-[url(/images/icons/iconCheckCircleBefore.png)] bg-white data-[state=checked]:bg-white data-[state=checked]:bg-[url(/images/icons/iconCheckCircleAfter.png)]'} />
               <Label htmlFor='termsAll' className={'font-semibold text-gr-900'}>필수 약관 전체 동의</Label>
             </div>
@@ -327,7 +324,7 @@ export default function JoinPage() {
               {
                 terms.map((term, idx) => {
                   return (
-                    <div key={idx} className={'flex items-center w-full gap-10 pl-20'}>
+                    <div key={idx} className={'flex items-center w-full gap-10 pl-20 cursor-pointer'}>
                       <Checkbox id={'terms' + idx} checked={term.checked} onCheckedChange={() => (checkboxOnChangeHandler(idx))} className={'w-20 h-20 rounded-full bg-center border-0 bg-[url(/images/icons/iconCheckCircleBefore.png)] bg-white data-[state=checked]:bg-white data-[state=checked]:bg-[url(/images/icons/iconCheckCircleAfter.png)]' } />
                       <Label htmlFor={'terms' + idx}>{term.text}</Label>
                     </div>
@@ -341,7 +338,7 @@ export default function JoinPage() {
       {/* 하단 영역 */}
       <div>
         <div className="fixed bottom-0 left-0 w-full h-117">
-          <div className={`mx-auto flex h-full min-w-320 max-w-575 justify-center bg-bg px-20 pt-16`}>
+          <div className={`mx-auto flex h-full min-w-320 max-w-575 justify-center bg-white px-20 pt-16`}>
             <Button
               size="full"
               className={isStepBtnActive ? `${activeBtnSt}` : `${disabledBtnSt}`}
