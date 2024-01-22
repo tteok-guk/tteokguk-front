3fdebfbe09490ea4ed0234f6869a66d1b2692e5d
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-22 22:19:19 +0900

    [TG-134] logout 모달 portal 적용
    Co-authored-by: Shinheeje <107026311+Shinheeje@users.noreply.github.com>

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -3,13 +3,30 @@
 import { createPortal } from 'react-dom'
 import { useEffect, useState } from 'react'
 import { ModalProps, ModalComponentType } from '@/types/CommonTypes'
-import { RouletteModal } from '../modal'
+import { RouletteModal, LogoutModal } from '../modal'
 
-export default function Modal({ type, cancelClick, confirmClick }: ModalProps) {
+export default function Modal({
+  type,
+  cancelClick,
+  title,
+  cancelBtnTitle,
+  confirmTitle,
+  cancelBtnFn,
+  confirmBtnFn,
+}: ModalProps) {
   const [mounted, setMounted] = useState(false)
 
   const componentType: ModalComponentType = {
     roulette: <RouletteModal cancelClick={cancelClick} />,
+    logout: (
+      <LogoutModal
+        title={title}
+        cancelBtnTitle={cancelBtnTitle}
+        confirmTitle={confirmTitle}
+        cancelBtnFn={cancelBtnFn}
+        confirmBtnFn={confirmBtnFn}
+      />
+    ),
   }
   const componentToRender = componentType[type]
   const modalRoot = document.getElementById('modal-root') as HTMLElement
