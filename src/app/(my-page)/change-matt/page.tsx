582f3d04cb2964991d942bcf968a2ce8d489d8d4
shinheeje <dbfk2637@naver.com>
2024-01-26 10:26:54 +0900

    Feat : CS모달 수정

--- src/app/(my-page)/change-matt/page.tsx
+++ src/app/(my-page)/change-matt/page.tsx
@@ -8,11 +8,13 @@ import { useState } from 'react'
 import { putMatt } from '@/services/changeMatt'
 import { useMutation } from '@tanstack/react-query'
 import { RequestParamType } from '@/types/apiTypes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useToast } from '@/hooks/use-toast'
 import { sampleDish } from '../../../../public/images/dishes'
 
 export default function ChangeMattPage() {
+  const params = useSearchParams()
+  const mattName = params.get('matt')
   const [chosenMatt, setChosenMatt] = useState('blueDew')
   const setMatt = (clickedValue: string) => setChosenMatt(clickedValue)
   const router = useRouter()
@@ -33,29 +35,31 @@ export default function ChangeMattPage() {
   return (
     <>
       <div className="h-dvh">
-        <div
-          className={`mx-[-20px] mt-[-32px] h-[55%] bg-[url(/images/matts/${chosenMatt}.png)] bg-cover bg-no-repeat px-20 pt-32`}
-        >
-          <TopButton />
-          <p className="font-xl mb-10">
-            변경할 테이블 매트를 <br /> 선택해 주세요
-          </p>
-          <div className="absolute left-1/2 -translate-x-1/2">
-            <Image src={sampleDish} alt="dish image" className="" />
+        <div className="content-height">
+          <div
+            className={`mx-[-20px] mt-[-32px] h-[55%] bg-[url(/images/matts/${chosenMatt}.png)] bg-cover bg-no-repeat px-20 pt-32`}
+          >
+            <TopButton />
+            <p className="font-xl mb-10">
+              변경할 테이블 매트를 <br /> 선택해 주세요
+            </p>
+            <div className="absolute left-1/2 -translate-x-1/2">
+              <Image src={sampleDish} alt="dish image" className="" />
+            </div>
           </div>
-        </div>
-        <div className="mt-20 grid basis-1/4 grid-cols-4 grid-rows-2 gap-12">
-          {AllMatt.map((matt, idx) => (
-            <button key={idx} onClick={() => setMatt(matt.id)}>
-              <Image
-                src={matt.src}
-                alt={matt.alt}
-                className={`m-auto rounded-lg mt-0${
-                  chosenMatt === matt.id ? 'rounded-lg border-3 border-pr-500' : ''
-                }`}
-              />
-            </button>
-          ))}
+          <div className="mt-20 grid basis-1/4 grid-cols-4 grid-rows-2 gap-12">
+            {AllMatt.map((matt, idx) => (
+              <button key={idx} onClick={() => setMatt(matt.id)}>
+                <Image
+                  src={matt.src}
+                  alt={matt.alt}
+                  className={`m-auto rounded-lg mt-0${
+                    chosenMatt === matt.id ? 'rounded-lg border-3 border-pr-500' : ''
+                  }`}
+                />
+              </button>
+            ))}
+          </div>
         </div>
         <BottomButton fullBtnName="완료" fullBtnClick={completeBtn} />
       </div>
--- src/components/modal/UserTestModal.tsx
+++ src/components/modal/UserTestModal.tsx
@@ -12,27 +12,29 @@ interface UserTestProps {
 function UserTestModal({ cancelBtnFn, confirmBtnFn }: UserTestProps) {
   return (
     <div className="modal-bg">
-      <div className="modal-wrapper">
-        <div className="flex items-end">
-          <div className="relative w-full rounded-t-2xl bg-pr-200 p-20">
-            <button className="absolute right-20 top-20" onClick={cancelBtnFn}>
-              <Image src={iconClose} alt="닫기 버튼" width={20} height={20} />
+      <div className="relative mx-auto flex h-full min-w-320 max-w-575 items-end">
+        <div
+          className="absolute bottom-0 right-0 top-0 w-full cursor-pointer bg-black bg-opacity-70"
+          onClick={cancelBtnFn}
+        ></div>
+        <div className="relative bottom-0 w-full rounded-t-2xl bg-pr-200 p-20">
+          <button className="absolute right-20 top-20" onClick={cancelBtnFn}>
+            <Image src={iconClose} alt="닫기 버튼" width={20} height={20} />
+          </button>
+          <div className="mt-30 p-20">
+            <Image src={userTest} alt="유저테스트 캐릭터" loading="eager" />
+          </div>
+          <Link
+            href={'https://forms.gle/oavk3yJND7A8JrMh7'}
+            target="_blank"
+            className="flex-center h-60 w-full rounded-md bg-pr-500 text-lg text-white"
+          >
+            <p>유저테스트 참여하기</p>
+          </Link>
+          <div className="flex-center mt-12">
+            <button className="w-100 text-pr-400" onClick={confirmBtnFn}>
+              다시 보지 않기
             </button>
-            <div className="mt-30 p-20">
-              <Image src={userTest} alt="유저테스트 캐릭터" />
-            </div>
-            <Link
-              href={'https://forms.gle/oavk3yJND7A8JrMh7'}
-              target="_blank"
-              className="flex-center h-60 w-full rounded-md bg-pr-500 text-lg text-white"
-            >
-              <p>유저테스트 참여하기</p>
-            </Link>
-            <div className="flex-center mt-12">
-              <button className="w-100 text-pr-400" onClick={confirmBtnFn}>
-                다시 보지 않기
-              </button>
-            </div>
           </div>
         </div>
       </div>
