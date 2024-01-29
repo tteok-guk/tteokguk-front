44061e5880e6520d1b66bbc164ec25b4bdfb5fed
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-29 09:35:34 +0900

    Remove : sentry 패키지 삭제

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withSentryConfig } = require('@sentry/nextjs')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -10,22 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  sentry: {
-    hideSourceMaps: true,
-  },
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+module.exports = nextConfig
