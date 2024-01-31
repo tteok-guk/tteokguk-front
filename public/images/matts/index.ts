31cb15fe94a6abbbe67d0185575039f768422d36
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-01 00:27:35 +0900

    Fix : pink 매트 파일명 카멜케이스로 통일+ (TG-34)

--- public/images/matts/index.ts
+++ public/images/matts/index.ts
@@ -1,11 +1,3 @@
-import miniBlueColCheck from './miniBlueColCheck.png'
-import miniBlueDew from './miniBlueDew.png'
-import miniBlueGradation from './miniBlueGradation.png'
-import miniBubblePattern from './miniBubblePattern.png'
-import miniPinkDew from './miniPinkDew.png'
-import miniPinkGradation from './miniPinkGradation.png'
-import miniPurpleCheck from './miniPurpleCheck.png'
-import miniYellowCheck from './miniYellowCheck.png'
 import blueColCheck from './blueColCheck.png'
 import blueDew from './blueDew.png'
 import blueGradation from './blueGradation.png'
@@ -14,16 +6,16 @@ import pinkDew from './pinkDew.png'
 import pinkGradation from './pinkGradation.png'
 import purpleCheck from './purpleCheck.png'
 import yellowCheck from './yellowCheck.png'
+import miniBlueColCheck from './miniBlueColCheck.png'
+import miniBlueDew from './miniBlueDew.png'
+import miniBlueGradation from './miniBlueGradation.png'
+import miniBubblePattern from './miniBubblePattern.png'
+import miniPinkDew from './miniPinkDew.png'
+import miniPinkGradation from './miniPinkGradation.png'
+import miniPurpleCheck from './miniPurpleCheck.png'
+import miniYellowCheck from './miniYellowCheck.png'
 
 export {
-  miniBlueColCheck,
-  miniBlueDew,
-  miniBlueGradation,
-  miniBubblePattern,
-  miniPinkDew,
-  miniPinkGradation,
-  miniPurpleCheck,
-  miniYellowCheck,
   blueColCheck,
   blueDew,
   blueGradation,
@@ -32,4 +24,12 @@ export {
   pinkGradation,
   purpleCheck,
   yellowCheck,
+  miniBlueColCheck,
+  miniBlueDew,
+  miniBlueGradation,
+  miniBubblePattern,
+  miniPinkDew,
+  miniPinkGradation,
+  miniPurpleCheck,
+  miniYellowCheck,
 }
