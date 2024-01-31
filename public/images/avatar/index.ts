b080c43c5f39e8d580b85e180517963bf2d7f1d1
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-01-31 23:48:56 +0900

    Design : 미사용 이미지 파일 정리 (TG-34)

--- public/images/avatar/index.ts
+++ public/images/avatar/index.ts
@@ -1,7 +1,6 @@
 import cryingAvatars from './cryingAvatars.svg'
 import speechBubble from './speechBubble.png'
-import captureInfo from './captureInfo.png'
 import userTest from './userTest.png'
 import characterSet from './characterSet.svg'
 
-export { cryingAvatars, speechBubble, captureInfo, userTest, characterSet }
+export { cryingAvatars, speechBubble, userTest, characterSet }
