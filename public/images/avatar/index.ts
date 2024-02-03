7929f99f6ad00aedaecc6f58a5c0edb45747aa74
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-03 11:24:59 +0900

    Style : 떡국 이미지 변경 (TG-145)
    Co-Authored-By: 신주영 <58963027+godjooyoung@users.noreply.github.com>

--- public/images/avatar/index.ts
+++ public/images/avatar/index.ts
@@ -1,7 +1,7 @@
+import characterSet from './characterSet.png'
 import cryingAvatars from './cryingAvatars.svg'
 import speechBubble from './speechBubble.png'
 import userTest from './userTest.png'
-import characterSet from './characterSet.svg'
 import myPageBottom from './myPageBottom.png'
 
-export { cryingAvatars, speechBubble, userTest, characterSet, myPageBottom }
+export { characterSet, cryingAvatars, speechBubble, userTest, myPageBottom }
