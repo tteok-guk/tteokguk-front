95ec3af8feec6e51020932c3e43f271a8adcc398
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-15 08:46:01 +0900

    [hotfix/TG-193] 로그인없이 구경하기 경로 변경 및 redirect 설정
    * Feat : 로그인없이 구경하기 경로 변경
    * Feat : 이전 공개 URL 주소 redirects() 설정
    
    ---------
    
    Co-authored-by: Shinheeje <107026311+Shinheeje@users.noreply.github.com>
    Co-authored-by: 신주영 <58963027+godjooyoung@users.noreply.github.com>
    Co-authored-by: stella0905 <109677566+stella0905@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/e75c43ec-3ec3-42bf-8b7a-7d96a0f7909b',
+        destination: '/1549cae9-b407-482f-b511-71efdb1951ea?page=1',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
